1cb88874c1988828f06b2a2e646c484fc171333c
Tim McCulloch <tim@mevu.bet>
2018-03-14 14:24:24 -0400

    updating migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,29 @@
 //const SafeMath = artifacts.require('./math/SafeMath.sol');
-const Ownable = artifacts.require('./ownership/Ownable.sol');
+//const Ownable = artifacts.require('./ownership/Ownable.sol');
 const Mevu = artifacts.require("./build/Mevu.sol");
 const Admin = artifacts.require("./build/Admin.sol");
 const Events = artifacts.require("./build/Events.sol");
 const Wagers = artifacts.require("./build/Wagers.sol");
+const WagersController = artifacts.require("./build/WagersController.sol");
+const CustomWagers = artifacts.require("./build/CustomWagers.sol");
+const CustomWagersController = artifacts.require("./build/CustomWagersController.sol");
+const Rewards = artifacts.require("./build/Rewards.sol");
 const OracleVerifier = artifacts.require("./build/OracleVerifier.sol");
 const Oracles = artifacts.require("./build/Oracles.sol");
-const Rewards = artifacts.require("./build/Rewards.sol");
+const OraclesController = artifacts.require("./build/OraclesController.sol");
+const MvuToken = artifacts.require("./build/MvuToken.sol");
+const CancelController = artifacts.require("./build/CancelController.sol");
+
 const BigNumber = require('bignumber.js');
 
 module.exports = (deployer, network, accounts) => {
     //  let totalSupply, minimumGoal, minimumContribution, maximumContribution, deployAddress, start, hours, isPresale, discounts;
     let deployAddress = accounts[0];    
-    //let totalSupply = 50;
+    let totalSupply = 500000000000;
   
 
      //deployer.deploy(SafeMath, {from: deployAddress});
-     deployer.deploy(Ownable, {from: deployAddress});
+    // deployer.deploy(Ownable, {from: deployAddress});
 
      //deployer.link(Ownable, [MvuToken, MvuSale], {from: deployAddress});
     // deployer.link(SafeMath, [MvuToken, MvuSale], {from: deployAddress});
@@ -33,14 +40,20 @@ module.exports = (deployer, network, accounts) => {
     //         discounts.map(v => new BigNumber(v)),
     //         {from: deployAddress});
     // });
-    deployer.deploy(Rewards,  {from: deployAddress, gas:7700000});
-    deployer.deploy(Oracles,  {from: deployAddress, gas:7700000});
-    deployer.deploy(Admin,  {from: deployAddress, gas:7700000});
-    deployer.deploy(Events,  {from: deployAddress, gas:7700000});
-    deployer.deploy(OracleVerifier,  {from: deployAddress, gas:7700000});
-    deployer.deploy(Wagers,  {from: deployAddress, gas:7700000});
-    deployer.deploy(Mevu,  {from: deployAddress, gas:9000000, value:100000000000000000});
-   // deployer.deploy(MvuToken, totalSupply,  {from: deployAddress});
+    
+    deployer.deploy(Oracles,  {from: deployAddress, gas:9000000});
+    deployer.deploy(OraclesController,  {from: deployAddress, gas:9000000});
+    deployer.deploy(Admin,  {from: deployAddress, gas:9000000});
+    deployer.deploy(Events,  {from: deployAddress, gas:9000000});
+    deployer.deploy(OracleVerifier,  {from: deployAddress, gas:9000000});
+    deployer.deploy(Wagers,  {from: deployAddress, gas:9000000});
+    deployer.deploy(WagersController,  {from: deployAddress, gas:9000000});
+    deployer.deploy(CustomWagers,  {from: deployAddress, gas:9000000});
+    deployer.deploy(CustomWagersController,  {from: deployAddress, gas:9000000});
+    deployer.deploy(CancelController,  {from: deployAddress, gas:9000000});
+    deployer.deploy(Rewards, {from: deployAddress, gas:9000000});
+    deployer.deploy(Mevu,  {from: deployAddress, gas:9000000, value:9100000000000000000});
+    deployer.deploy(MvuToken, totalSupply,  {from: deployAddress});
 
     //  deployer.deploy(MvuToken, totalSupply,  {from: deployAddress}).then(() => {
       
